773a30570b350bb823f937394e999f685859aea3
pneha1234 <nehapandey408@gmail.com>
2020-08-15 16:17:49 +0545

    templates

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Neha'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
