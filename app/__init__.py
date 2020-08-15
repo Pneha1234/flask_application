0c96a03545485f6a71fb1694d39b2436a931d172
pneha1234 <nehapandey408@gmail.com>
2020-08-15 19:37:04 +0545

    migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from app.config import Config
 
 app = Flask(__name__)
-FLASK_DEBUG=1
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
