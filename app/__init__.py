50cca7f5dd856172c6b4ae30a2b0040c0faa8bc7
pneha1234 <nehapandey408@gmail.com>
2020-08-16 10:08:00 +0545

    add applog files

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,8 @@
+import logging
+import os
+from datetime import datetime
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -12,4 +17,17 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('/logs/microblog.log'.format(datetime.utcnow()), maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+from app import routes, models, errors
