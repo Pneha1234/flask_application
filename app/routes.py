ea1cd3644615182ec2e29c89f5a8059a210cce7d
pneha1234 <nehapandey408@gmail.com>
2020-08-16 09:02:20 +0545

    recording the last visit time for a user

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
@@ -73,3 +75,10 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
